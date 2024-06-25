fe080c2aa6d134bc2d8cecf9a25e40be858942d7
pugal1510 <108538519+pugal1510@users.noreply.github.com>
2024-06-25 11:57:44 +0530

    Add files via upload

---
+++ H2database/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,31 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
